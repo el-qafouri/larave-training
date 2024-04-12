79648ee50b8f637d427af729392c3474c75251fa
ahmad mohammadi <ahmadmohammadi940@gmail.com>
2024-04-12 19:41:17 +0330

    Complete verify tests

--- tests/Feature/API/V1/Auth/VerifyTest.php
+++ tests/Feature/API/V1/Auth/VerifyTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Feature\API\V1\Auth;
 
+use App\Events\UserVerified;
+use App\Listeners\UpdateUserVerification;
 use App\Models\User;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Event;
 use Tests\TestCase;
 
 class VerifyTest extends TestCase
@@ -62,7 +65,7 @@ class VerifyTest extends TestCase
         ])
             ->assertOk();
 
-        //or fake the cache
+        //2. or fake the cache
         //        Cache::put($user->mobile, $code);
 
         $data = [
@@ -78,5 +81,64 @@ class VerifyTest extends TestCase
             'mobile' => $data['mobile'],
             'email_verified_at' => null,
         ]);
+    }
+
+    public function test_that_events_have_dispatched_successfully()
+    {
+        $code = 1111;
+        $user = User::factory()
+            ->create();
+
+        $this->postJson('api/v1/auth/login', [
+            'mobile' => $user->mobile,
+        ])
+            ->assertOk();
+
+        $data = [
+            'mobile' => $user->mobile,
+            'code' => strval($code),
+        ];
+        Event::fake();
+
+        $this->postJson($this->url, $data)
+            ->assertOk();
+
+        Event::assertDispatched(UserVerified::class);
+        Event::assertListening(UserVerified::class,
+            UpdateUserVerification::class,
+        );
+    }
+
+    public function test_that_cache_has_cleared_after_successful_verify()
+    {
+        $code = 1111;
+        $user = User::factory()
+            ->create();
+        $data = [
+            'mobile' => $user->mobile,
+            'code' => strval($code),
+        ];
+        //if set as doesnt exists
+        //        Cache::shouldReceive('has')->once()->andReturnFalse();
+        //        Cache::shouldReceive('put')->once()
+        //            ->with($data['mobile'], \Mockery::any(), config('services.auth.verification_token_ttl'))
+        //            ->andReturnTrue();
+        //if exists
+        Cache::shouldReceive('has')->once()->andReturnTrue();
+        Cache::shouldReceive('get')->once()
+            ->with($data['mobile'])
+            ->andReturn($code);
+        $this->postJson('api/v1/auth/login', [
+            'mobile' => $user->mobile,
+        ])
+            ->assertOk();
+
+        Cache::shouldReceive('get')->once()->andReturn($code);
+        Cache::shouldReceive('forget')->once()
+            ->with($data['mobile']);
+
+        $this->postJson($this->url, $data)
+            ->assertOk();
+
     }
 }
